e6f555a13145d6fe0753c5c12445d27409dd5922
Sávio S. Dias <soares-go@MacBook-de-Savio-Soares.local>
2013-04-22 00:25:10 -0300

    [Added] Graph classe
    Adicionado classe de representação de um grafo

---
+++ Graph.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+# -*- encoding: utf-8 -*-
+
+class Graph:
+	'''
+	Classe de representação de um grafo
+	'''
+
+	def __init__( self ):
+		self.vertexes = []
+		self.edges = []
+
+	def __init__( self, vertexes, edges ):
+		self.vertexes = vertexes
+		self.edges = edges
+
+	def __repr__( self ):
+		pass
+
+	def adjacencyMatrix( self ):
+		adjacencyMatrix = []
